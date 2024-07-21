f4d68fefc52c722334f3683d656a274a4b949e90
AlexInABox <main@alexinabox.de>
2024-07-21 20:44:32 +0200

    only start anim when all images are loaded

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-const startAnim = () => {
-    console.log("Starting background animation");
-    document.querySelector("main").classList.add("animatedMain");
+var counter = 0;
+const queueAnim = () => {
+    counter++;
+    console.log("Loaded " + counter + "/4 images");
+    if (counter === 4) {
+        console.log("All images loaded");
+        document.querySelector("main").classList.add("animatedMain");
+    }
 }
