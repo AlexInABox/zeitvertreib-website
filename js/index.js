f157a03508b8faac33850f18a2bca5b01e2bdcf2
radston12 <radston12@gmail.com>
2024-07-22 13:52:53 +0200

    Remove old preloading

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var counter = 0;
-const queueAnim = () => {
-    counter++;
-    console.log("Loaded " + counter + "/4 images");
-    if (counter === 4) {
-        console.log("All images loaded");
-        document.querySelector("main").classList.add("animatedMain");
-    }
-}
