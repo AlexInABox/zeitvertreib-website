2521e1d8784a6f2153c2d8aa4cf45a51a1a3aaad
Alexander Betke <main@alexinabox.de>
2025-04-06 23:26:51 +0200

    fix: correct access to kills property in response body for user statistics

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -101,9 +101,7 @@ export class DashboardComponent {
     }).subscribe({
       next: response => {
         if (response.status === 200 && response.body) {
-          console.log(response.body);
-          console.log((response.body as any).kills);
-          if ((response.body as any).kills)
+          if ((response.body.stats as any).kills)
             this.userStatistics = { ...response.body.stats };
           else {
             this.userStatistics.username = response.body.stats.username;
