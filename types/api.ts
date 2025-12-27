5a61aceae095b420c4e7d33b920fed7a4b76d629
AlexInABox <AlexInABox@users.noreply.github.com>
2025-12-27 09:21:36 +0000

    Prettified Code!

--- types/api.ts
+++ types/api.ts
@@ -235,7 +235,7 @@ export interface UserFakerank {
 }
 
 /** GET /fakerank-admin/user response */
-export interface GetUserFakerankResponse extends UserFakerank { }
+export interface GetUserFakerankResponse extends UserFakerank {}
 
 /** POST /fakerank-admin/user request */
 export interface SetUserFakerankRequest {
