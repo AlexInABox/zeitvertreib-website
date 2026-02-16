c138aad84e949b3bfa22b1c05bcb453e33617daf
AlexInABox <AlexInABox@users.noreply.github.com>
2026-02-16 08:48:44 +0000

    Prettified Code!

--- frontend/src/app/services/easter-egg.service.ts
+++ frontend/src/app/services/easter-egg.service.ts
@@ -3,10 +3,10 @@ import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EasterEggService {
-    /** Observable that emits when chiikawa easter egg should be triggered */
-    chiikawaTrigger$ = new Subject<void>();
+  /** Observable that emits when chiikawa easter egg should be triggered */
+  chiikawaTrigger$ = new Subject<void>();
 
-    triggerChiikawa(): void {
-        this.chiikawaTrigger$.next();
-    }
+  triggerChiikawa(): void {
+    this.chiikawaTrigger$.next();
+  }
 }
