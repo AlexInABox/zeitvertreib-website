929502674278340d2ee0b3b94b51ce2a44637b7e
AlexInABox <hobby@alexinabox.de>
2026-05-03 14:45:18 +0200

    feat: add the worst punishment

---
+++ Flipped/Events/TheWorstPunishment.cs
@@ -0,0 +1,21 @@
+using LabApi.Features.Wrappers;
+using MEC;
+
+namespace Flipped.Events;
+
+public class TheWorstPunishment : IEvent
+{
+    public EventType EventType { get; } = EventType.Cruel;
+
+    public bool CanRun(Player player)
+    {
+        return true;
+    }
+
+    public void Run(Player player)
+    {
+        EventHandlers.PushUserMessage(player,
+            "Die Münze verbannt dich in die Hölle. Einem der schlimmsten Orte auf diesem Planeten...");
+        Timing.CallDelayed(5f, () => { player.RedirectToServer(7200); });
+    }
+}
