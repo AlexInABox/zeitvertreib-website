7e8339602e502926f637cd518d70e37d076404f1
AlexInABox <hobby@alexinabox.de>
2026-05-06 14:12:55 +0200

    chore: remove dead code

--- Eventim/Events/CoinGluttony.cs
+++ Eventim/Events/CoinGluttony.cs
@@ -1,14 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
-using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.Handlers;
 using LabApi.Features.Wrappers;
 using MEC;
-using PlayerRoles;
-using PlayerRoles.PlayableScps.Scp3114;
-using Respawning.Objectives;
-using UnityEngine;
-using Logger = LabApi.Features.Console.Logger;
 
 namespace Eventim.Events;
 
@@ -58,51 +52,5 @@ public class CoinGluttony : IEvent
 
             yield return Timing.WaitForSeconds(2f);
         }
-    }
-
-    private static void OnPlayerSpawned(PlayerSpawnedEventArgs ev)
-    {
-        // SCP173 is super unfair in this game mode! That's why we disable their spawning :3
-        if (ev.Role is { ServerSpawnReason: RoleChangeReason.RoundStart, RoleTypeId: RoleTypeId.Scp173 })
-            Timing.CallDelayed(Timing.WaitForOneFrame, () =>
-            {
-                Logger.Info($"Setting {ev.Player.DisplayName} to Scientist because they would've been SCP 173");
-                ev.Player.SetRole(RoleTypeId.Scientist, RoleChangeReason.RoundStart);
-            });
-
-
-        // DisableAllEffects doest remove the initial night vision of SCP's, and I cant figure out how to do that!
-        // in-game I would click "Clear all effects" on an scp and then give them NightVision. But this code here doesnt simulate that :shrug:
-        /*
-        if (ev.Player is { IsSCP: true, Role: not RoleTypeId.Scp079 })
-        {
-            ev.Player.DisableAllEffects();
-            ev.Player.EnableEffect<NightVision>(intensity: 4);
-        }
-        */
-        Timing.RunCoroutine(ShowAura(ev.Player));
-    }
-
-
-    private static IEnumerator<float> ShowAura(Player player)
-    {
-        if (player.Role is RoleTypeId.Scp079) yield break;
-        LightSourceToy auraLight = LightSourceToy.Create(player.GameObject!.transform);
-        auraLight.Color = player.Role.GetRoleColor();
-        auraLight.Range = 5f;
-        auraLight.Intensity = 0.95f;
-        auraLight.ShadowType = LightShadows.None;
-
-        while (player.IsAlive)
-        {
-            // This allows SCP3114 to also copy their victims auraLight!
-            Color auraColor = player.Role.GetRoleColor();
-            if (player.RoleBase is Scp3114Role { Disguised: true } scp3114Role)
-                auraColor = scp3114Role.CurIdentity.StolenRole.GetRoleColor();
-            auraLight.Color = auraColor;
-            yield return Timing.WaitForSeconds(1f);
-        }
-
-        auraLight.Destroy();
     }
 }
