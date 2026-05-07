cb965efa425deb4ddac3d04120de70ae50e76cb4
AlexInABox <hobby@alexinabox.de>
2026-05-07 09:00:06 +0200

    feat: now tracking round #

--- Tracked/Config.cs
+++ Tracked/Config.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace Tracked;
 
 public class Config
@@ -5,6 +7,11 @@ public class Config
     public bool Debug { get; set; } = false;
 
     public int CoinEscapeMultiplier { get; set; } = 3;
+
+    [Description(
+        "The # of the current round. (Or next round if the server is already waiting for players!) The value increments by itself but can be manually adjusted to create an offset.")]
+    public int CurrentRoundNumber { get; set; } = 0;
+
     public string EndpointUrl { get; set; } = "https://example.com/upload";
     public string Apikey { get; set; } = "1234";
 }
