cd54985e7d1fa19b044fcd33041124f8c4971a4f
AlexInABox <hobby@alexinabox.de>
2026-07-25 13:42:28 +0200

    the return of the squished plugin

---
+++ Squished/Config.cs
@@ -0,0 +1,10 @@
+namespace Squished;
+
+public class Config
+{
+    public bool Debug { get; set; } = false;
+
+    public string BonkSoundEffectPath { get; set; } =
+        "/home/container/.config/SCP Secret Laboratory/LabAPI/configs/7100/Squished/bonk.ogg";
+    public float Volume { get; set; } = 2f;
+}
